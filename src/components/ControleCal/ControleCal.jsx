9386f2b3e86b0e5975ebda46fb1ba6a0f476418b
Vitor Musolino Teixeira <160746719+vitormusolino@users.noreply.github.com>
2024-12-09 09:26:50 -0300

    Add files via upload

--- src/components/ControleCal/ControleCal.jsx
+++ src/components/ControleCal/ControleCal.jsx
@@ -4,7 +4,7 @@ import ImgCelulares from '../../assets/ImgCelulares.png'
 
 function ControleCal() {
     return ( <>
-                <div className='flex py-10 mx-72 px-10 justify-around items-center gap-20'>
+                <div className='flex py-10 mx-72 justify-around items-center gap-10'>
                     <div>
                         <h1 className='text-5xl font-bold mb-5'>Controle suas calorias com somente uma foto!</h1>
                         <p className='text-base max-w-lg text-cinza-texto font-medium mb-5'>Conheça o Fit Cal, o app inteligente que torna mais fácil acompanhar suas calorias. Basta tirar uma foto ou escanear um código de barras, e ele calcula na hora as calorias e os nutrientes. Com insights personalizados e lembretes inteligentes, você se mantém no foco e alcança suas metas de forma mais rápida!</p>

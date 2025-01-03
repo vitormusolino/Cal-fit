15ee3b469017a31c8e0e55cd7f68ebeb01bb8f98
Vitor Musolino Teixeira <160746719+vitormusolino@users.noreply.github.com>
2025-01-03 17:20:13 -0300

    Add files via upload

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,24 +2,34 @@ import { Link } from 'react-router-dom';
 
 function Footer() {
     return (
-        <>
-            <footer className="my-20">
+        <footer className="my-20">
+            <p className="text-center text-sm text-slate-500">
+                Copyright © 2024 Fit Cal
+            </p>
+            <p className="text-center text-xs text-slate-500 mt-3">
+                <Link to="/termos" className="mx-2 hover:underline">
+                    Termos de Serviço
+                </Link>
+                |
+                <Link to="/privacidade" className="mx-2 hover:underline">
+                    Políticas de Privacidade
+                </Link>
+            </p>
+            <div className="flex flex-col items-center mt-4">
                 <p className="text-center text-sm text-slate-500">
-                    Copyright © 2024 Fit Cal
+                    Contato:
                 </p>
-                <p className="text-center text-xs text-slate-500 mt-3">
-                    <Link to="/termos" className="mx-1 hover:underline">Termos de Serviço</Link> |
-                    <Link to="/privacidade" className="mx-1 hover:underline">Políticas de Privacidade</Link> 
-                </p>
-                <div className='flex flex-col justify-center'>
-                    <p className='text-center text-sm text-slate-500 mt-3'>Contato:</p>
-                    <div className='flex justify-center mt-2'>
-                            <a href="mailto:fittcalapp@gmail.com" className='text-center text-xs text-slate-500 mx-2 hover:underline'> fittcalapp@gmail.com </a>
-                            <p className='text-center text-xs text-slate-500 mx-2'> +55 11 96853-3521</p>
-                    </div>
+                <div className="flex flex-col sm:flex-row items-center sm:justify-center mt-2 space-y-2 sm:space-y-0 sm:space-x-4">
+                    <a
+                        href="mailto:fittcalapp@gmail.com"
+                        className="text-xs text-slate-500 hover:underline"
+                    >
+                        fittcalapp@gmail.com
+                    </a>
+                    <p className="text-xs text-slate-500">+55 11 96853-3521</p>
                 </div>
-            </footer>
-        </>
+            </div>
+        </footer>
     );
 }
 
